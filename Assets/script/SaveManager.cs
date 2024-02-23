8db051ef78f8681d86e0739fa0bf81bd5daf119a
hoonah137 <121156401+hoonah137@users.noreply.github.com>
2024-02-23 09:51:06 +0100

    done

--- Assets/script/SaveManager.cs
+++ Assets/script/SaveManager.cs
@@ -15,6 +15,24 @@ public class SaveManager : MonoBehaviour
     [SerializeField] int _userScore;
     [SerializeField] Vector3 _userPosition;
 
+    [SerializeField] Transform _player;
+
+    [SerializeField] Transform _checkPoint1;
+    [SerializeField] Transform _checkPoint2;
+    [SerializeField] Transform _checkPoint3;
+    [SerializeField] Transform _checkPoint4;
+
+    [SerializeField] float _checkradius = 2f;
+
+    bool _checked1;
+    bool _checked2;
+    bool _checked3;
+    bool _checked4;
+
+
+
+
+
 
 
     // Start is called before the first frame update
@@ -22,6 +40,35 @@ public class SaveManager : MonoBehaviour
     {
         LoadData();
         
+        _checked1 = Physics.CheckSphere(_checkPoint1.position, _checkradius);
+        _checked2 = Physics.CheckSphere(_checkPoint2.position, _checkradius);
+        _checked3 = Physics.CheckSphere(_checkPoint3.position, _checkradius);
+        _checked4 = Physics.CheckSphere(_checkPoint4.position, _checkradius);
+        
+        
+    }
+
+    
+    void Update()
+    {
+        if (_checked1 == true)
+        {
+            LoadData();
+        }
+
+        if (_checked2 == true)
+        {
+            LoadData();
+        }
+
+        if (_checked3 == true)
+        {
+            LoadData();
+        }
+        if (_checked4 == true)
+        {
+            LoadData();
+        }
     }
 
     public void SaveData()
